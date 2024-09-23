66151ae1428de2cb25cd4ce4ebfffc37828777c9
AmanTarar <aman.tarar@chicmic.co.n>
2024-09-23 14:31:08 +0530

    feat(user route): added new routes for update delete

--- app/routes/v1/userRoutes.js
+++ app/routes/v1/userRoutes.js
@@ -28,9 +28,68 @@ module.exports = [
             model: "loginUser",
             body: {
                 email: Joi.string().pattern(EMAIL_REGEX).required(),
-                password: Joi.string().pattern(PASSWORD_PATTER_REGEX)
+                password: Joi.string().pattern(PASSWORD_PATTER_REGEX).required()
             }
         },
         handler: userController.login
+    },
+    {
+        method: "PUT",
+        path: "/api/user",
+        auth: true,
+        joiSchemaForSwagger: {
+            group: "User",
+            description: "Update user",
+            model: "updateUser",
+            body: {
+                name: Joi.string().pattern(NAME_REGEX).required()
+            },
+            headers: {
+                authorization: Joi.string().required()
+            }
+        },
+        handler: userController.updateUser
+    },
+    {
+        method: "GET",
+        path: "/api/user",
+        auth: true,
+        joiSchemaForSwagger: {
+            group: "User",
+            description: "Get user",
+            model: "getUser",
+            headers: {
+                authorization: Joi.string().required()
+            }
+        },
+        handler: userController.getUser
+    },
+    {
+        method: "POST",
+        path: "/api/logout",
+        auth: true,
+        joiSchemaForSwagger: {
+            group: "User",
+            description: "Logout user",
+            model: "logoutUser",
+            headers: {
+                authorization: Joi.string().required()
+            }
+        },
+        handler: userController.logout
+    },
+    {
+        method: "DELETE",
+        path: "/api/user",
+        auth: true,
+        joiSchemaForSwagger: {
+            group: "User",
+            description: "Delete user",
+            model: "DeleteUser",
+            headers: {
+                authorization: Joi.string().required()
+            }
+        },
+        handler: userController.deleteUser
     }
 ]
